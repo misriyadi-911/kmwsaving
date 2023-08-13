59065bef45cc711e956ba587f4a8d800d2fd528e
Muhammad Umar Mansyur <umar.ovie@gmail.com>
2023-08-13 11:40:05 +0700

    Push

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,9 +4,13 @@
 namespace App\Http\Controllers;
 
 use App\Events\ChatEvent;
+use Illuminate\Support\Facades\Broadcast;
 
 class ChatController extends Controller {
   public function test_pusher() {
+    Broadcast::channel('chat', function ($user) {
+      return true;
+    });
     event(new ChatEvent('Hi'));
   }
 }
